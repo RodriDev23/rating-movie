10dffca1f472c01a4070128e3073009bbe0ced94
RodriDev23 <rodrigoprogramador23@outlook.com>
2023-09-25 16:50:20 -0400

    Adding navbar and testing it

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,8 +1,47 @@
+import React from 'react';
 
 export default function NavBar() {
   return (
-    <div>
-
-    </div>
-  )
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="text-white text-2xl font-semibold">Movie Rating App</div>
+          <ul className="flex space-x-4">
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+              >
+                Top Rated
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+              >
+                My Ratings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+              >
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
   <div className="flex flex-col justify-center items-center min-h-screen min-w-screen">
-    
+  <NavBar />
 
   </div>
     )
