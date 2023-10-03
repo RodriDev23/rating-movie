e0e48f9c2850c48a2c1643108202a5541ecb1473
EzequielAgnelli <ezequielkawaii@outlook.com>
2023-10-03 16:44:29 -0300

    apiKey-commit

--- pages/components/SectionOne.tsx
+++ pages/components/SectionOne.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const apiKey = '../api/ApiKey.tsx'
+export default apiKey;
 
 export function SingleMovieSection() {
+    const [movieData, setMovieData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://www.omdbapi.com/?t=Marvel&apikey=${apiKey}`);
+                const data = await response.json();
+                setMovieData(data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+
+    }, []);
+
     return (
 
         <section className="p-10 w-[80%] flex flex-col justify-center items-center md:max-w-2xl">
@@ -8,7 +27,7 @@ export function SingleMovieSection() {
                 Most New
             </h4>
         <div className="rounded-lg p-5 bg-slate-900 w-[850px] flex flex-col justify-center items-center">
-            <img src="https://placehold.co/330x450" alt="name of the movie" className='mt-10 rounded-xl' />
+            <img src="{MovieData.Poster}" alt="{MovieData.Title}" className='mt-10 rounded-xl' />
         <h5
         className="mb-2 text-xl font-bold leading-tight text-center py-5">
         Title of the Movie
