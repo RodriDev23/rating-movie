cf1f287c4c979cf652d0c4593909568963153b27
EzequielAgnelli <ezequielkawaii@outlook.com>
2023-10-11 14:01:49 -0300

    new-section added

--- pages/components/FooterSection.tsx
+++ pages/components/FooterSection.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// import movieIcon from ''
+
 
 export function Footer() {
     return (
@@ -6,7 +8,7 @@ export function Footer() {
             <div className="max-w-screen-x1 mx-auto p-4 md:py-8 ">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://github.com/RodriDev23/rating-movie" className="flex items-center mb-4 sm:mb-0" target="_blank">
-                <img src="https://placehold.co/64x64" alt="Movie Icon" />
+                {/* <img src={} alt="Movie Icon" /> */}
                 <span className="self-center text-2xl font-bold whitespace-nowrap text-white ml-4">Our Repository</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
