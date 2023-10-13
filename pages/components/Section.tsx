2069d2f327dc598538e180f510e35fd360182612
EzequielAgnelli <ezequielkawaii@outlook.com>
2023-10-13 15:06:35 -0300

    new cards added + styles

--- pages/components/Section.tsx
+++ pages/components/Section.tsx
@@ -10,11 +10,11 @@ export const Section : React.FC<titlePrincipal> = ({tittle}) => {
   const { data, loading } = useFetch({query: 'spider-man'}); // Pass the query string here
 
   if (loading) {
-    return <div>Loading...</div>; // Add a loading indicator if needed
+    return <div className="text-lime-500 font-bold">Loading...</div>; // Add a loading indicator if needed
   }
 
   if (!data) {
-    return <div>Error fetching data.</div>; // Handle error state if the request fails
+    return <div className="text-red-600 font-bold">Error fetching data.</div>; // Handle error state if the request fails
   }
 
   const {
@@ -25,19 +25,19 @@ export const Section : React.FC<titlePrincipal> = ({tittle}) => {
   } = data;
 
   return (
-    <section className="p-10 w-[80%] flex flex-col justify-center items-center md:max-w-2xl gap-10">
-      <h4 className="pr-5 pb-5 font-bold text-left uppercase text-xl">
+    <section className=" bg-slate-900 p-2 flex flex-col justify-center items-center md:max-w-2xl gap-2 rounded-lg">
+      <h4 className="pr-2 pb-4 font-bold text-left uppercase text-xl">
         {tittle}
       </h4>
-      <div className="rounded-lg p-5 bg-slate-900 w-[850px] flex flex-col justify-center items-center gap-10">
-        <img src={Poster} alt={Title} className='mt-10 rounded-xl w-20 gap-2' />
-        <h5 className="mb-2 text-xl font-bold leading-tight text-center py-5">
+      <div className="rounded-lg p-5 w-[450px] flex flex-col justify-center items-center gap-5">
+        <img src={Poster} alt={Title} className='mt-0 rounded-xl gap-2' />
+        <h5 className="mb-2 text-xl font-bold leading-tight text-center py-2">
           {Title}
         </h5>
-        <span className="mb-4 text-sm text-white text-right font-semibold">
+        <span className="text-sm text-white text-right font-semibold">
           {Released}
         </span>
-        <p className="mb-4 text-base text-white text-center p-5 font-semibold">
+        <p className="text-base text-white text-center p-2 font-semibold">
           {Plot}
         </p>
       </div>
