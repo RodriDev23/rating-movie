4db70f6bc106e025fba32425845b17fb8cde2721
RodriDev23 <rodrigoprogramador23@outlook.com>
2023-10-15 08:37:58 -0400

    changes test

--- pages/components/SectionOne.tsx
+++ pages/components/SectionOne.tsx
@@ -13,6 +13,7 @@ export const SectionsOne : React.FC<TitleSecondSection> = ({ttitle}) => {
         {ttitle}
         <div className="flex flex-col border-solid border border-sky-600 mt-5 w-full gap-5"></div>
       </h4>
+<<<<<<< HEAD
       <div className='flex justify-center items-center flex-wrap gap-6 '>
           <SectionMostNew ttitle="" />
           <SectionMostNew ttitle="" />
@@ -20,6 +21,19 @@ export const SectionsOne : React.FC<TitleSecondSection> = ({ttitle}) => {
           <SectionMostNew ttitle="" />
           <SectionMostNew ttitle="" />
           <SectionMostNew ttitle="" />
+=======
+      <div className="rounded-lg p-5 bg-slate-900 w-[850px] flex flex-col justify-center items-center">
+        <img src={Poster} alt={Title} className='mt-10 rounded-xl' />
+        <h5 className="mb-2 text-xl font-bold leading-tight text-center py-4">
+          {Title}
+        </h5>
+        <span className="mb-4 text-sm text-white text-right font-semibold">
+          {Released}
+        </span>
+        <p className="mb-4 text-base text-white text-center p-5 font-semibold">
+          {Plot}
+        </p>
+>>>>>>> 54810ff (changes test)
       </div>
     </section>
   );
